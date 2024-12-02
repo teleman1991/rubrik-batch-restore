55669d0c979dd4d0faec3e224925ebed4880e500
teleman1991 <168704451+teleman1991@users.noreply.github.com>
2024-12-01 21:12:45 -0600

    Update script for specific SharePoint Online tenant

--- batch_sharepoint_restore.py
+++ batch_sharepoint_restore.py
@@ -2,9 +2,14 @@ import rubrik_cdm
 
 rubrik = rubrik_cdm.Connect(node_ip='my.rubrik.cluster')
 
-sharepoint_sites = rubrik.get('v1', '/sharepoint/site')
+SHAREPOINT_TENANT = 'compassdatacenter.sharepoint.com'
+
+sharepoint_sites = rubrik.get('v1', '/sharepoint/site', params={'detail': 'true'})
 
 for site in sharepoint_sites['data']:
-    print('Restoring SharePoint site: {}'.format(site['name']))
-    restore_job = rubrik.post('v1', '/sharepoint/site/{}/restore'.format(site['id'])) 
-    print('Restore job started for {}, status: {}'.format(site['name'], restore_job['status']))
+    if SHAREPOINT_TENANT in site['url']:
+        print('Restoring SharePoint site: {}'.format(site['name']))
+        restore_job = rubrik.post('v1', '/sharepoint/site/{}/restore'.format(site['id'])) 
+        print('Restore job started for {}, status: {}'.format(site['name'], restore_job['status']))
+    else:
+        print('Skipping site not in tenant: {}'.format(site['name']))
